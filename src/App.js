2cacfebc5201305fc629d11955886287c1f29ad2
Peterkim88 <hyunilpeter.kim@gmail.com>
2022-10-04 00:56:34 -0400

    productpage udpated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import CustomerHeader from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/Homepage";
+import ProductPage from "./pages/Productpage";
 
 function App() {
   return (
-    <div>
+    <Router>
       <CustomerHeader />
+
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
+
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Container, Navbar, Nav, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function CustomerHeader() {
   return (
     <header>
       <Navbar bg="primary" variant='dark' expand="lg" collapseOnSelect >
         <Container>
-          <Navbar.Brand href="/">Quick</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Quick</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user'></i> Login</Nav.Link>
+
+              <LinkContainer to="/cart">
+                <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/login'>
+                <Nav.Link><i className='fas fa-user'></i> Login</Nav.Link>
+              </LinkContainer>
+
             </Nav>
           </Navbar.Collapse>
         </Container>
